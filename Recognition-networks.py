138d3950feac0203169e119eb3e0af17919ed2ee
Daniel Luque <dluque-6@outlook.com>
2021-05-21 02:02:22 +0100

    Improve classes
    Improve classes by defining fields and methods required for the Gillespie algorithm
    - Death rate of a clonotype remains undefined

--- Recognition-networks.py
+++ Recognition-networks.py
@@ -3,34 +3,42 @@ import pickle
 import numpy as np
 from copy import deepcopy
 from itertools import chain, combinations
-from random import uniform
+from random import seed, uniform
 
-num_clonotypes = 5  # Number of clonotypes
-num_peptides = 30  # Number of peptides
-peptide_prob_value = 5 / 30  # Probability that a peptide will be recognised by a random clonotype
+seed('A random string to generate a seed that allows us to reproduce the same network each time')
+num_clonotypes = 4  # Number of clonotypes
+num_peptides = 10  # Number of peptides
+birth_rate_value = 1.0
+peptide_stimulus_value = 10.0
+peptide_prob_value = 2/5  # Probability that a peptide will be recognised by a random clonotype
+realisations = 1  # Number of realisations
+sharing_factor = 1.5
 
 
 class Peptide:
-    """Class for peptide objects"""
+    """Class to represent peptides"""
 
-    def __init__(self, probability_value, position_value):
+    def __init__(self, probability_value, position_value, stimulus_value):
         """
         Parameters
         ----------
         probability_value : float
-            Probability of peptide being recognised
+            Probability of peptide being recognised.
         position_value : int
             Number of the peptide.
+        stimulus_value : float
+            Stimulus provided by the peptide.
         """
 
         self.probability = probability_value
         self.position = position_value
+        self.stimulus = stimulus_value
         self.clonotypes = []
         self.recognised = 0
 
     def add_clonotype(self, clonotype):
         """
-        Adds a clonotype to the list of clonotypes that recognise the peptide, and increases *recognised* by 1.
+        Adds a clonotype to the list of clonotypes that recognise the peptide and increases *recognised* by 1.
 
         Parameters
         ----------
@@ -42,11 +50,33 @@ class Peptide:
             self.clonotypes.append(clonotype.position)
             self.recognised += 1
 
+    def cell_stimulus(self, clonotype_list):
+        """
+        Calculates the stimulus provided to a cell that recognises the peptide.
+
+        Parameters
+        ----------
+        clonotype_list : list[Clonotype]
+            Current list of clonotypes.
+
+        Returns
+        -------
+        float
+            Stimulus provided to a cell that recognises the peptide
+        """
+
+        cells = 0
+
+        for i in self.clonotypes:
+            cells += clonotype_list[i].cells
+
+        return self.stimulus / cells
+
 
 class Clonotype:
-    """Class for clonotype objects"""
+    """Class to represent clonotypes"""
 
-    def __init__(self, num_cells, position_value):
+    def __init__(self, num_cells, position_value, br_value):
         """
         Parameters
         ----------
@@ -54,23 +84,28 @@ class Clonotype:
             Number of initial cells.
         position_value : int
             Number of the clonotype.
+        br_value : float
+            Birth rate for a cell in homeostasis
         """
 
         self.cells = num_cells
         self.position = position_value
+        self.homeostatic_br = br_value
         self.peptides = []
+        self.recognised = 0
 
     def add_peptide(self, peptide_list, subset=None):
         """
-        Checks if peptides in *peptide_list* will be recognised by the clonotype,
-        and appends them to *peptides* if they are.
+        Checks if peptides in *peptide_list* will be recognised by the clonotype.
+
+        If *subset* is included checks if the peptides in the subset will be recognised with an increased probability.
 
         Parameters
         ----------
         peptide_list : list[Peptide]
-            Peptide considered.
+            Peptides considered.
         subset : list[int]
-            Subset of
+            Subset of peptides with increased recognition probability.
         """
 
         if subset is None:
@@ -79,10 +114,46 @@ class Clonotype:
                     check_value = uniform(0.0, 1.0)
                     if check_value < peptide.probability:
                         self.peptides.append(peptide.position)
+                        self.recognised += 1
                         peptide.add_clonotype(self)
+        elif len(subset) > 0:
+            for index in subset:
+                if peptide_list[index].position not in self.peptides:
+                    check_value = uniform(0.0, 1.0)
+                    if check_value < sharing_factor * peptide_list[index].probability:
+                        self.peptides.append(peptide_list[index].position)
+                        self.recognised += 1
+                        peptide_list[index].add_clonotype(self)
+
+    def birth_rate(self, clonotype_list, peptide_list):
+        """
+        Calculates the rate at which a new cell of the clonotype is born.
+
+        Parameters
+        ----------
+        clonotype_list : list[Clonotype]
+            Current list of clonotypes.
+        peptide_list : list[Peptide]
+            Peptides considered.
+
+        Returns
+        -------
+        float
+            Birth rate for clone in state.
+        """
+
+        foreign = 0.0
+        for peptide in peptide_list:
+            if peptide.position in self.peptides:
+                foreign += peptide.cell_stimulus(clonotype_list)
+
+        return self.cells * (self.homeostatic_br + foreign)
 
     def birth(self):
         self.cells += 1
+
+    def death(self):
+        self.cells -= 1
 
     def n_death_check(self, num_deaths):
         if self.cells < num_deaths:
@@ -92,3 +163,26 @@ class Clonotype:
 
     def n_death(self, num_deaths):
         self.cells -= num_deaths
+
+
+clones = [Clonotype(5, i) for i in range(num_clonotypes)]
+peptides = [Peptide(peptide_prob_value, i) for i in range(num_peptides)]
+
+for clone in clones:
+    clone.add_peptide(peptides)
+
+seed()
+
+states = []
+times = []
+
+for current_realisation in range(realisations):
+    current_states = [[clone.cells for clone in clones]]
+    current_times = [0]
+
+    # Gillespie stuff
+    while 1 < 0:
+        pass
+
+    states.append(current_states)
+    times.append(current_times)
